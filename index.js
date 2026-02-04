3b36fd3aa2646377b46c36196a251ae6d8f1b9f9
Albert-nhc <1745257998@qq.com>
2026-02-04 15:03:15 +0800

    index.js

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
+const dogGif = document.getElementById("dogGif");
 const optionA = document.getElementById("optionA");
 const optionB = document.getElementById("optionB");
-const dogGif = document.getElementById("dogGif");
 
-// 所有可随机的 gif
-const gifList = [
-  "02.gif",
-  "03.gif",
-  "04.gif",
-  "05.gif"
-];
+// 所有可用的 gif
+const gifs = ["01.gif", "02.gif", "03.gif", "04.gif", "05.gif", "06.gif", "07.gif", "08.gif"];
 
-
-// ⭐ 抽一个随机 gif 的函数
-function changeGifRandomly() {
-  const randomIndex = Math.floor(Math.random() * gifList.length);
-  dogGif.src = gifList[randomIndex];
+function randomGif() {
+  const index = Math.floor(Math.random() * gifs.length);
+  dogGif.src = gifs[index];
 }
 
-// 点击 A
-optionA.addEventListener("click", () => {
+function selectA() {
   optionA.classList.add("active");
   optionA.classList.remove("inactive");
 
   optionB.classList.add("inactive");
   optionB.classList.remove("active");
 
-  // 换 gif
-  changeGifRandomly();
-});
+  randomGif();
+}
 
-// 点击 B
-optionB.addEventListener("click", () => {
+function selectB() {
   optionB.classList.add("active");
   optionB.classList.remove("inactive");
 
   optionA.classList.add("inactive");
   optionA.classList.remove("active");
 
-  // ⭐ 同样换 gif
-  changeGifRandomly();
-});
+  randomGif();
+}
+
+optionA.addEventListener("click", selectA);
+optionB.addEventListener("click", selectB);
+
